7bb26fbb054df7c71dad3eb8d85ed9b260d5df46
CueBaZe <olivervalente@icloud.com>
2026-04-08 10:30:46 +0200

    Made getAccessToken function

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect} from 'react';
+import redirectToAuthCodeFlow from './auth';
 
 interface Song {
   id: string;
@@ -11,45 +12,43 @@ interface Song {
 
 export default function App() {
   const [searchInput, setSearchInput] = useState<string>("");
-  const [token, setToken] = useState<string>("");
+  const [token, setToken] = useState<string>(window.localStorage.getItem('token') || "");
   const [songs, setSongs] = useState<Song[]>([]);
 
-  const getAccessToken = async () => {
-    const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID; //Gets the Client ID from the env file
-    const clientSecret = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET; //Get the Client Secret from the env file
-    const url = 'https://accounts.spotify.com/api/token'; //spotify's api endpoint to get access token
+  const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
+  const code = undefined;
 
-    const params = new URLSearchParams();
-    params.append('grant_type', 'client_credentials'); //tells the api is an app
-    params.append('client_id', clientId);
-    params.append('client_secret', clientSecret);
+  if (!code) {
+    redirectToAuthCodeFlow(clientId);
+  } else {
+    const accessToken = 
+  }
 
-    try {
-      const response = await fetch(url, { //fetches the token
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: params,
-      });
-
-      const data = await response.json();
-      if (data.access_token) {
-        setToken(data.access_token);
-      }
-    } catch (error) {
-      console.error('Fetch failed:', error);
-    }
-  };
 
   useEffect(() => { //runs on mount
-    getAccessToken();
+    const hash = window.location.hash;
+    
+    if (hash) {
+      const _token = hash
+        .substring(1)
+        .split('&')
+        .find(elem => elem.startsWith('access_token'))
+        ?.split('=')[1];
+
+        if (_token) {
+          setToken(_token);
+
+          window.localStorage.setItem('token', _token)
+
+          window.location.hash = '';
+        }
+    }
   }, []);
 
   const updateSongList = async (name: string) => {
     setSearchInput(name);
 
-    if (!name.trim()) {
+    if (!token || !name.trim()) {
     setSongs([]);
     return;
   }
@@ -78,32 +77,42 @@ export default function App() {
       <div className='flex flex-col min-h-screen w-screen bg-[#232423] items-center gap-[50px] pt-10'>
         <h1 className='text-center text-6xl font-bold text-green-300'>Spotify test app</h1>
         
-        <div className="flex flex-col items-center gap-2">
-          <input 
-            type='text' 
-            placeholder='What do you want to listen to?' 
-            className='border-2 border-[#363636] rounded-2xl w-[350px] text-xl text-white p-2 bg-transparent'
-            value={searchInput}
-            onChange={(e) => updateSongList(e.target.value)}
-          />
-          <p className="text-gray-400 text-sm">
-            {token ? "✅ API Connected" : "❌ Connecting to Spotify..."}
-          </p>
-          <div className='grid grid-cols-12 text-center gap-[30px] p-4'>
-              {/* Loops throug the songs and makes the box foreach song */}
-              {songs.map((song) => ( 
-                <div className='flex flex-col col-span-4 items-center border border-1 border-[#363636] rounded-lg p-2' key={song.id}> 
-                  <img 
-                    src={song.album.images[1]?.url} 
-                    alt={song.name} 
-                    className='rounded-xl mb-4'
-                  />
-                  <h1 className='text-white font-bold text-xl'>{song.name}</h1>
-                  <p className='text-white text-md'>{song.artists[0].name}</p>
-                </div>
-              ))}
+        {token && (
+          <div className="flex flex-col items-center gap-2">
+            <input 
+              type='text' 
+              placeholder='What do you want to listen to?' 
+              className='border-2 border-[#363636] rounded-2xl w-[350px] text-xl text-white p-2 bg-transparent'
+              value={searchInput}
+              onChange={(e) => updateSongList(e.target.value)}
+            />
+            <p className="text-gray-400 text-sm">
+              {token ? "✅ API Connected" : "❌ Connecting to Spotify..."}
+            </p>
+            <div className='grid grid-cols-12 text-center gap-[30px] p-4'>
+                {/* Loops throug the songs and makes the box foreach song */}
+                {songs.map((song) => ( 
+                  <div className='flex flex-col col-span-4 items-center border border-1 border-[#363636] rounded-lg p-2' key={song.id}> 
+                    <img 
+                      src={song.album.images[1]?.url} 
+                      alt={song.name} 
+                      className='rounded-xl mb-4'
+                    />
+                    <h1 className='text-white font-bold text-xl'>{song.name}</h1>
+                    <p className='text-white text-md'>{song.artists[0].name}</p>  
+                  </div>
+                ))}
+            </div>
           </div>
-        </div>
+        )}
+
+        {!token && (
+          <div>
+            <a href={loginUrl}>
+              Login
+            </a>
+          </div>
+        )}
       </div>
     </section>
   );
